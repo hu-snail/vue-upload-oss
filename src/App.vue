6dbd4eb540af3f335862a91d9f1f864baa64de7e
“hu-snail” <1217437592@qq.com>
2022-06-10 22:44:30 +0800

    feat: 新增首页布局

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <el-container class="container">
+      <el-header class="header">
+        <div class="left">
+          <img width="16" height="16" src="./assets/moon.svg" class="moon" alt="Active darkmode" />
+          <a href="/" class=""><p class="dark-transition dark:text-white text-blue-400 text-xl">hu-snail.dev</p></a>
+        </div>
+      </el-header>
+      <el-main class="main">
+        <div class="main-head-wrapper">
+            <h2 class="h2-title"> vue-upload-oss </h2>
+            <div>
+              <h1 class="title">
+                A modern <span class="bg-clip-text	linear-gradient inverse text-fill-transparent">Vue2</span> 
+                <br/>
+                Collocation
+                <span class="bg-clip-text	linear-gradient inverse text-fill-transparent">Element-ui</span>  &
+                <span class="bg-clip-text	linear-gradient inverse text-fill-transparent">Ali-oss</span> 
+              </h1>
+            </div>
+        </div>
+      </el-main>
+    </el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
+* {
+  margin: 0;
+  padding: 0;
+}
+.container {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  /* background: #151621;     */
+  height: 100vh;
+}
+.header {
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #ddd;
+}
+.header .left {
+  display: flex;
+  align-items: center;
+}
+.header .left .moon {
+  margin-right: 5px;
+  cursor: pointer;
+}
+.main-head-wrapper {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.main-head-wrapper .h2-title {
+  padding: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Container, Button, Header, Main } from 'element-ui';
+import './styles/index.css'
 
 Vue.config.productionTip = false
+
+Vue.component(Button.name, Button);
+Vue.component(Header.name, Header);
+Vue.component(Main.name, Main);
+Vue.component(Container.name, Container);
 
 new Vue({
   render: h => h(App),
